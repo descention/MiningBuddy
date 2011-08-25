72fb70a101f1bc2cb17ddc81eba436eff8b91136
darth.arnold@thedarth.com <darth.arnold@thedarth.com@2859372d-cc67-f5c2-90e2-32f137dab868>
2011-08-25 02:48:27 +0000

    Update footer.php to show new issue tracking icon and add new URLs to project site.

--- Mining/include/html/footer.php
+++ Mining/include/html/footer.php
@@ -10,9 +10,9 @@
 <tr><td>
 <p align="left">%%IMG%%</p>
 </td><td align="right">
-<a href="http://bugs.whistle-britches.com/"><img src="./images/bugs.gif" width="88" height="32" border="0" alt="MiningBuddy Bug Tracking System" /></a>
-<a href="http://miningbuddy.whistle-britches.com"><img src="images/mblogo.gif" width="87" height="30" border="0" alt="MiningBuddy Project Site" /></a>
-<a href="http://miningbuddy.net/page.php?7"><img src="./images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" /></a>
+<a href="http://code.google.com/p/mining-buddy-plus/issues/list"><img src="./images/codeissue.png" width="88" height="32" border="0" alt="Mining-Buddy-Plus Bug Tracking System" /></a>
+<a href="http://miningbuddy.thedarth.com"><img src="images/mblogo.gif" width="87" height="30" border="0" alt="Mining-Buddy-Plus Project Site" /></a>
+<!-- <a href="http://miningbuddy.net/page.php?7"><img src="./images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" /></a> -->
 </td></tr>
 </table>
 </body>
