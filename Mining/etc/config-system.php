7d5041f314a051ff9f022400ef448bc60084f501
darth.arnold@thedarth.com <darth.arnold@thedarth.com@2859372d-cc67-f5c2-90e2-32f137dab868>
2011-12-26 15:20:04 +0000

    update version number

--- Mining/etc/config-system.php
+++ Mining/etc/config-system.php
@@ -39,7 +39,7 @@
  * installation.
  */
 
-$VERSION_COMP = "0.8.7.41";
+$VERSION_COMP = "0.8.8.50";
 $VERSION = "MiningBuddy Plus " . $VERSION_COMP;
 
 $DSN = "$mysql_protocol://$mysql_username:$mysql_password@$mysql_hostname/$mysql_dbname";
