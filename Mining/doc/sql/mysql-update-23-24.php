a1cffb7a0685a21512c26f000a563542e4bcdc4a
darth.arnold@thedarth.com <darth.arnold@thedarth.com@2859372d-cc67-f5c2-90e2-32f137dab868>
2011-12-26 20:19:34 +0000

    spelling Errors

--- Mining/doc/sql/mysql-update-23-24.php
+++ Mining/doc/sql/mysql-update-23-24.php
@@ -29,7 +29,7 @@ function Getbasesite($file)
 
 if ($upgrade=='1' ){
 	
-	echo "Add Tables for Lottory";
+	echo "Add Tables for Lottery";
 	echo "<br>";
 	$configupgrade0 = mysql_query("CREATE TABLE IF NOT EXISTS `lotteryTickets` (`id` int(5) NOT NULL AUTO_INCREMENT, `ticket` int(5) NOT NULL, `drawing` int(4) NOT NULL, `owner` int(5) NOT  NULL DEFAULT '-1', `isWinner` tinyint(1) NOT NULL DEFAULT '0', PRIMARY KEY (`id`)) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;");
 	$configupgrade1 = mysql_query("CREATE TABLE IF NOT EXISTS `lotto` (`drawing` int(5) NOT NULL AUTO_INCREMENT, `opened` int(12) NOT NULL, `closed` int(12) NOT NULL, `isOpen` tinyint(1) NOT NULL DEFAULT '0', `winningTicket` int(5) DEFAULT NULL, `winner` int(5) DEFAULT NULL, `potSize` int(8) DEFAULT NULL, PRIMARY KEY (`drawing`)) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=19 ;");
@@ -52,7 +52,7 @@ echo "Updating the config version number!";
 ?>
 <center>
 This is the Mining Buddy SQL upgrade page.<br>
-When you are ready to upgrade you Mining Buddy Install to the newest version click the link below<br>
+When you are ready to upgrade your Mining Buddy Install to the newest version click the link below<br>
 <a href="mysql-update-23-24.php?upgrade=1">Upgrade Now</a>
 <?
 }
