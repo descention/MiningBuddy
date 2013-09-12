1d3dd6dcf4426d0f49fdaa5e6265862f66142562
Scott Mundorff <scott.mundorff@gmail.com>
2013-09-12 10:47:57 -0400

    footer update

--- include/html/footer.php
+++ include/html/footer.php
@@ -10,9 +10,8 @@
 <tr><td>
 <p align="left">%%IMG%%</p>
 </td><td align="right">
-<a href="http://code.google.com/p/mining-buddy-plus/issues/list"><img src="./images/codeissue.png" width="88" height="32" border="0" alt="Mining-Buddy-Plus Bug Tracking System" /></a>
-<a href="http://miningbuddy.thedarth.com"><img src="images/mblogo.gif" width="87" height="30" border="0" alt="Mining-Buddy-Plus Project Site" /></a>
-<!-- <a href="http://miningbuddy.net/page.php?7"><img src="./images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" /></a> -->
+<a href="https://github.com/descention/MiningBuddy/issues"><img src="./images/codeissue.png" width="88" height="32" border="0" alt="Wormhole Buddy Bug Tracking System" /></a>
+<a href="https://github.com/descention/MiningBuddy"><img src="images/mblogo.gif" width="87" height="30" border="0" alt="Wormhole Buddy Project Site" /></a>
 </td></tr>
 </table>
 </body>
